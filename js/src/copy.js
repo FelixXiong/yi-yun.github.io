759d560130935251a7ac1a498a83fc4650312a32
yi-yun <tp91225@163.com>
2018-09-16 11:29:09 +0800

    Site updated: 2018-09-16 11:28:59

--- js/src/copy.js
+++ js/src/copy.js
@@ -1,31 +1,24 @@
-function addLink() {
-    var body_element = document.getElementsByTagName('body')[0];
-    var selection;
-if(window.getSelection){//DOM,FF,Webkit,Chrome,IE10
-selection = window.getSelection();
-alert("文字复制成功！若有文字残缺请用右键复制\n转载请注明出处："+document.location.href);
- 
-}else if(document.getSelection){//IE10
-selection= document.getSelection();
-alert("文字复制成功！若有文字残缺请用右键复制\n转载请注明出处："+document.location.href);
- 
-}else if(document.selection){//IE6+10-
-selection= document.selection.createRange().text;
-alert("文字复制成功！若有文字残缺请用右键复制\n转载请注明出处："+document.location.href);
-}else{
-selection= "";
-alert("浏览器兼容问题导致复制失败！");
-}
-    var pagelink = "<br /><br /> 转载请注明来源: <a href='"+document.location.href+"'>"+document.location.href+"</a>"; 
-    var copy_text = selection + pagelink;
-    var new_div = document.createElement('div');
-    new_div.style.left='-99999px';
-    new_div.style.position='absolute';
-    body_element.appendChild(new_div );
-    new_div.innerHTML = copy_text ;
-    selection.selectAllChildren(new_div );
-    window.setTimeout(function() {
-        body_element.removeChild(new_div );
-    },0);
-}
-document.body.oncopy = addLink;
+function addLink() 
+{ 
+    var body_element = document.body; var selection;
+    selection = window.getSelection(); 
+    if (window.clipboardData) { // Internet Explorer 
+        var pagelink ="\r\n\r\n 原文出自[ Yi-Yun博客 ](yi-yun.github.io) \n除特别声明外，均采用CC BY-NC-SA 4.0许可协议\n转载请保留原文链接: "+document.location.href+""; 
+        var copytext = selection + pagelink;
+        window.clipboardData.setData ("Text", copytext); 
+        return false; 
+    }
+    else {
+        var pagelink = "<br/>原文出自[ Yi-Yun博客 ](yi-yun.github.io)<br/>除特别声明外，均采用 CC BY-NC-SA 4.0 许可协议<br/>转载请保留原文链接: "+document.location.href+"";
+        var copytext = selection + "<br/>"+pagelink;
+        var newdiv = document.createElement('div');
+        newdiv.style.position='absolute';
+        newdiv.style.left='-99999px';
+        body_element.appendChild(newdiv);
+        newdiv.innerHTML = copytext;
+        selection.selectAllChildren(newdiv);
+        window.setTimeout(function() { body_element.removeChild(newdiv);},0);
+    } 
+} 
+document.oncopy = addLink;
+
